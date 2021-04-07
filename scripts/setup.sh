afb854bc0934a8645d4d9885aa75ad4eb1a03844
Oliver Hathaway <oliver.hathaway@bjss.com>
2021-04-07 10:27:28 +0100

    Fixed Viz installation order

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -27,23 +27,21 @@ echo Installing Linkerd
 linkerd check --pre
 
 linkerd install | kubectl apply -f -
-linkerd viz install | kubectl apply -f -
 
 kubectl wait -n linkerd --for=condition=available deployment/linkerd-controller --timeout=120s
 
 linkerd check
 
-echo Installing Nginx
+echo Installing Nginx and Viz
 
 # installing ingress for kind
 linkerd inject https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml | kubectl apply -f -
 
+linkerd viz install | kubectl apply -f -
+
 echo Waiting for Nginx to finish startup with 5 min timeout
 
-kubectl wait --namespace ingress-nginx \
-  --for=condition=ready pod \
-  --selector=app.kubernetes.io/component=controller \
-  --timeout=300s
+kubectl wait -n ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=300s
 
 echo Running Skaffold
 
